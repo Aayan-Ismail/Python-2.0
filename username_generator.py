2bf43dc41704a9002b0521f2d47abea5209988fb
Aayan-Ismail <mohammed143ismail@gmail.com>
2025-08-23 17:56:32 +0100

    Add files via upload

---
+++ username_generator.py
@@ -0,0 +1,40 @@
+import random
+
+def username(full_name):
+    name_parts = full_name.split()
+    if len(name_parts) < 2:
+        return "Please enter your full name, which is your first and last name with a space inbetween"
+
+    first_name = name_parts[0].lower()
+    last_name = name_parts[-1].lower()
+
+    randomruser6 = str(random.randint(-50,50))
+    
+
+    #combination 1
+    username1 = (first_name + last_name)
+    username2 = (last_name + first_name)
+    username3 = (first_name + first_name)
+    username4 = (last_name + last_name)
+    username5 = first_name[:3] + last_name[:3] + first_name[4:6] + last_name[4:6]
+    username6 = (first_name + randomruser6 + last_name)
+
+    username = [username1 , username2 , username3 , username4 , username5 , username6]
+    return random.choice(username)
+
+while True:
+    full_name = input("type your first and last name to generate a username, type exit to exit the generator. ")
+    if full_name.lower() == "exit":
+        break
+    name = username(full_name)
+    print(name)
+    
+
+
+
+
+
+
+
+
+
